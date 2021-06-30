c1307b18bc0b03ac074fbf34b47b4e20c04da3c9
JMK92 <81665620+JMK92@users.noreply.github.com>
2021-06-30 21:46:35 +0900

    Add files via upload

---
+++ Study/7. ROS2/Day03/src/main.cpp
@@ -0,0 +1,10 @@
+#include "TeleopTurtle.h"
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<TeleopTurtle>());
+    rclcpp::shutdown();
+    
+    return 0;
+}
